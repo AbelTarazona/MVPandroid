63f9dd6e5fb93cd6d8c6f6e3ddfb4caa9b05032b
AbelTarazona <abel.tarazona@urp.edu.pe>
2020-01-10 12:43:06 -0500

    Se inicializo los componentes visuales

--- app/src/main/java/com/example/mvpexample/MainActivity.java
+++ app/src/main/java/com/example/mvpexample/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.mvpexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.mvpexample.Presenter.ILoginPresenter;
+import com.example.mvpexample.Presenter.LoginPresenter;
+import com.example.mvpexample.View.ILoginView;
+
+public class MainActivity extends AppCompatActivity implements ILoginView {
+
+    EditText edt_email, edt_password;
+    Button btnlogin;
+
+    ILoginPresenter loginPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init view
+        btnlogin = findViewById(R.id.btnLogin);
+        edt_email = findViewById(R.id.edt_email);
+        edt_password = findViewById(R.id.edt_password);
+
+        //init
+        loginPresenter = new LoginPresenter(this);
+
+        //event
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginPresenter.onLogin(edt_email.getText().toString(), edt_password.getText().toString());
+            }
+        });
+
+    }
+
+    @Override
+    public void onLoginResult(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
